6cb9bedc67b476d8d99c5c800d04dd06f7e7f4d1
MuzammilMeh <muzammil.mehmood@xloopdigital.com>
2023-09-12 02:38:40 +0500

    first commit

--- app.py
+++ app.py
@@ -18,136 +18,73 @@ import json
 from langchain.schema import SystemMessage
 from fastapi import FastAPI
 import streamlit as st
+from langchain.utilities import SerpAPIWrapper, GoogleSerperAPIWrapper
+from langchain.agents import Tool
+from langchain.tools.file_management.write import WriteFileTool
+from langchain.tools.file_management.read import ReadFileTool
+from langchain.vectorstores import FAISS
+from langchain.docstore import InMemoryDocstore
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_experimental.autonomous_agents import AutoGPT
+from langchain.vectorstores.base import VectorStore
+from langchain.document_loaders import TextLoader, DirectoryLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+
+import streamlit as st
+from langchain import LLMChain, OpenAI, PromptTemplate
+import os
+from langchain.chains import RetrievalQA, SimpleSequentialChain
+import faiss
 
 load_dotenv()
 brwoserless_api_key = os.getenv("BROWSERLESS_API_KEY")
 serper_api_key = os.getenv("SERP_API_KEY")
 
-# 1. Tool for search
+
+embeddings_model = HuggingFaceEmbeddings()
+# Initialize the vectorstore as empty
+
+embedding_size = 768
+index = faiss.IndexFlatL2(embedding_size)
+vectorstore = FAISS(embeddings_model.embed_query, index, InMemoryDocstore({}), {})
 
 
-def search(query):
-    url = "https://google.serper.dev/search"
-
-    payload = json.dumps({"q": query})
-
-    headers = {"X-API-KEY": serper_api_key, "Content-Type": "application/json"}
-
-    response = requests.request("POST", url, headers=headers, data=payload)
-
-    print(response.text)
-
-    return response.text
+loader = DirectoryLoader("customer_data", loader_cls=TextLoader)
+# loader = TextLoader("customer_data/comp_complaints.txt")
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+docs = text_splitter.split_documents(documents)
+docsearch = FAISS.from_documents(docs, embeddings_model)
 
 
-# 2. Tool for scraping
-def scrape_website(objective: str, url: str):
-    # scrape website, and also will summarize the content based on objective if the content is too large
-    # objective is the original objective & task that user give to the agent, url is the url of the website to be scraped
-
-    print("Scraping website...")
-    # Define the headers for the request
-    headers = {
-        "Cache-Control": "no-cache",
-        "Content-Type": "application/json",
-    }
-
-    # Define the data to be sent in the request
-    data = {"url": url}
-
-    # Convert Python object to JSON string
-    data_json = json.dumps(data)
-
-    # Send the POST request
-    post_url = f"https://chrome.browserless.io/content?token={brwoserless_api_key}"
-    response = requests.post(post_url, headers=headers, data=data_json)
-
-    # Check the response status code
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
-        text = soup.get_text()
-        print("CONTENTTTTTT:", text)
-
-        if len(text) > 10000:
-            output = summary(objective, text)
-            return output
-        else:
-            return text
-    else:
-        print(f"HTTP request failed with status code {response.status_code}")
-
-
-def summary(objective, content):
-    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
-
-    text_splitter = RecursiveCharacterTextSplitter(
-        separators=["\n\n", "\n"], chunk_size=10000, chunk_overlap=500
-    )
-    docs = text_splitter.create_documents([content])
-    map_prompt = """
-    Write a summary of the following text for {objective}:
-    "{text}"
-    SUMMARY:
-    """
-    map_prompt_template = PromptTemplate(
-        template=map_prompt, input_variables=["text", "objective"]
-    )
-
-    summary_chain = load_summarize_chain(
-        llm=llm,
-        chain_type="map_reduce",
-        map_prompt=map_prompt_template,
-        combine_prompt=map_prompt_template,
-        verbose=True,
-    )
-
-    output = summary_chain.run(input_documents=docs, objective=objective)
-
-    return output
-
-
-class ScrapeWebsiteInput(BaseModel):
-    """Inputs for scrape_website"""
-
-    objective: str = Field(
-        description="The objective & task that users give to the agent"
-    )
-    url: str = Field(description="The url of the website to be scraped")
-
-
-class ScrapeWebsiteTool(BaseTool):
-    name = "scrape_website"
-    description = "useful when you need to get data from a website url, passing both url and objective to the function; DO NOT make up any url, the url should only be from the search results"
-    args_schema: Type[BaseModel] = ScrapeWebsiteInput
-
-    def _run(self, objective: str, url: str):
-        return scrape_website(objective, url)
-
-    def _arun(self, url: str):
-        raise NotImplementedError("error here")
-
-
-# 3. Create langchain agent with the tools above
+customer_complaints = RetrievalQA.from_chain_type(
+    llm=OpenAI(temperature=0), chain_type="refine", retriever=docsearch.as_retriever()
+)
 tools = [
     Tool(
-        name="Search",
-        func=search,
-        description="useful for when you need to answer questions about current events, data. You should ask targeted questions",
+        name="Customer_complaints",
+        func=customer_complaints.run,
+        description="useful for when you need to answer questions about customer data and complaints. You should ask targeted questions",
     ),
-    ScrapeWebsiteTool(),
+    # Add other Tool instances as needed
 ]
 
+
 system_message = SystemMessage(
-    content="""You are a world class researcher, who can do detailed research on any topic and produce facts based results; 
+    content="""You are a Product Analyst responsible for analyzing customer conversations to identify issues and propose solutions. Your objective is to thoroughly examine customer feedback and interactions, seeking out problems and formulating data-backed solutions; 
             you do not make things up, you will try as hard as possible to gather facts & data to back up the research
             
             Please make sure you complete the objective above with the following rules:
-            1/ You should do enough research to gather as much information as possible about the objective
-            2/ If there are url of relevant links & articles, you will scrape it to gather more information
-            3/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 3 iteratins
-            4/ You should not make things up, you should only write facts & data that you have gathered
-            5/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
-            6/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research"""
+            1/ Carefully review customer conversations to extract relevant information about issues they are facing with our product
+            2/ Analyze the data for patterns and trends to identify recurring problems
+            3/ If there are URLs or relevant links in the conversations, visit these links to gather additional context and information." If answer is yes, continue; But don't do this more than 3 iteratins
+            4/ Consider if there are any additional research avenues that can enhance the quality of your analysis based on the data you have collected. Limit additional research to a maximum of 3 iterations.
+            5/ You should not make things up, you should only write facts & data that you have gathered
+            6/ Present only factual information and data that you have gathered during your analysis
+            7/ Focus on providing actionable insights that can lead to product improvements and better customer satisfaction.
+            """
 )
 
 agent_kwargs = {
@@ -172,13 +109,15 @@ agent = initialize_agent(
 
 # 4. Use streamlit to create a web app
 def main():
-    st.set_page_config(page_title="Futuresync research agent", page_icon=":bird:")
+    st.set_page_config(
+        page_title="Futuresync Product Analyst agent", page_icon=":bird:"
+    )
 
-    st.header("Futuresync research agent :bird:")
+    st.header("Futuresync Product Analyst agent :bird:")
     query = st.text_input("Research goal")
 
     if query:
-        st.write("Doing research for ", query)
+        st.write("Doing Product Analyses ", query)
 
         result = agent({"input": query})
 
@@ -187,19 +126,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-# 5. Set this as an API endpoint via FastAPI
-app = FastAPI()
-
-
-class Query(BaseModel):
-    query: str
-
-
-@app.post("/")
-def researchAgent(query: Query):
-    query = query.query
-    content = agent({"input": query})
-    actual_content = content["output"]
-    return actual_content
